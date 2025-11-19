2cea63b77ba0129f3598244b91b8bc78dc062929
orianaaldana-cell <jarapicasso@gmail.com>
2025-11-19 09:37:08 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
